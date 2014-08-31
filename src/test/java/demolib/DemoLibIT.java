09d6163752f5e6556f2f313490695a8da276a83c
Hazze <hans.hazelius@gmail.com>
2014-08-31 19:59:55 +0200

    Add DemoLib integration test case

---
+++ src/test/java/demolib/DemoLibIT.java
@@ -0,0 +1,28 @@
+package demolib;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DemoLibIT {
+	@Before
+	public void beforeMethod() {
+		assertTrue("Native lib needs to be loaded for tests",canLoadNativeLib());
+	}
+	private boolean canLoadNativeLib() {
+		try{
+			new DemoLib();
+			return true;
+		}catch (UnsatisfiedLinkError e){
+			return false;
+		}
+	}
+	@Test
+	public void getMessageQuotedShouldQuoteMessageNo10() throws Exception{
+		DemoLib demoLib = new DemoLib();
+		String messageQuoted = demoLib.getMessageQuoted(10);
+		assertEquals("do_or_do_not_there_is_no_try", messageQuoted);
+	}
+}
